c1acea8bfbc0f90542b5605d4485ea96523a76d2
michellechristina <michelle@thisisbam.com>
2017-10-02 20:52:26 -0400

    initial commit

---
+++ array.js
@@ -0,0 +1,21 @@
+//creates an empty array
+var cars= [];
+
+// put an item in the array
+cars.push ("toyota");
+cars.push("honda");
+cars.push("jeep");
+
+// view what's in the array
+console.log(cars);
+
+// view what's in the first position of the array
+console.log(cars[0]);
+
+// create an array of values
+var colors=["purple", "pink", "red"];
+
+// loop over an array and log it out
+for (var i=0; i<colors.length; i++) {
+    console.log(colors[i]);
+}
