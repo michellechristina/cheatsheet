e0fb2d86fe1f6eceb937765c6b43be553fd0e57e
michellechristina <michelle@thisisbam.com>
2017-10-02 21:13:54 -0400

    changes

--- array.js
+++ array.js
@@ -19,3 +19,36 @@ var colors=["purple", "pink", "red"];
 for (var i=0; i<colors.length; i++) {
     console.log(colors[i]);
 }
+
+// another way to loop over an array
+// forEach is a shortcut to do a for loop.
+// forEach is an array method (or function) that takes a function as its (argument)
+colors.forEach(function(color){
+    console.log(color);
+}) ;
+
+// create a function to show the benefit of forEach
+// this function takes in a string & changes it to uppercase, but it's not called back by anything yet
+function toUppercase(str){
+   return str.toUpperCase();
+}
+
+var uppercaseColors=colors.forEach(toUppercase);
+// returns undefined bc forEach doesn't return a value
+console.log(uppercaseColors);
+
+//this is a demo of array.map
+// array.map is like forEach, but it does return a value, which is another array
+
+var uppercaseColors=colors.map(toUppercase);
+console.log(uppercaseColors);
+
+// remove or "pop" something off the end of the array
+// LIFO - last in, first out
+console.log("\n-----------------------");
+console.log(colors);
+colors.pop();
+console.log(colors);
+var color=colors.pop();
+console.log("color: " + color);
+console.log("colors: " + colors);
